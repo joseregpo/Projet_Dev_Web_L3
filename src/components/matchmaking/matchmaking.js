1f583f783c26b6be52767b93e638bc8a8bfc1fe5
Rafinator07 <75440470+Rafinator07@users.noreply.github.com>
2023-03-31 14:57:49 +0200

    Create matchmaking.js

---
+++ src/components/matchmaking/matchmaking.js
@@ -0,0 +1,26 @@
+//Imports pour React
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux'; 
+//Imports pour Material UI
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+
+
+const Matchmaking = () => (
+  <div>
+    Matchmaking Component
+  </div>
+);
+
+Matchmaking.propTypes = {};
+
+Matchmaking.defaultProps = {};
+
+export default Matchmaking;
