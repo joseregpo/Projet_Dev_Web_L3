ee17659527f4a2b85f4b0c4f385ba21f03648336
LouisLangellier <louis.langellier@gmail.com>
2023-03-31 11:52:05 +0200

    Ajout de redux toolkit

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,14 @@
+import { useSelector } from 'react-redux'; 
+
 export default function Home(){
 
+    const user = useSelector(state => state);
+    console.log(user);
 
     return(
         <div>
             <h1>Faut faire un component navbar et le home</h1>
+            <h3>Personne connectée : {user.username}</h3>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux"; 
+import { store } from "./store";
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Auth from './components/auth';
@@ -26,9 +28,9 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
