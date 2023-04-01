77ca9f4ddd849b543fdc16c7f0c63d6b44266e81
LouisLangellier <louis.langellier@gmail.com>
2023-04-01 15:21:47 +0200

    New auth interface, logout function implemented

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,28 +1,43 @@
 import { React, useCallback, useRef, useState } from "react";
-import { EyeFill } from "react-bootstrap-icons";
-import { useNavigate } from 'react-router-dom';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { useDispatch } from 'react-redux'; 
-import { userConnect } from '../store'
+import { useNavigate } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
+import Box from "@mui/material/Box";
+import Stack from '@mui/material/Stack';
+import { useDispatch } from "react-redux";
+import { userConnect } from "../store";
 
 export default function Auth() {
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [error, setError] = useState("");
   const [register, setRegister] = useState(false);
-  const [visibility, setVisibility] = useState(false);
+  const [visibilityPassword, setVisibilityPassword] = useState(false);
+  const [visibilityConfirmPassword, setVisibilityConfirmPassword] = useState(false);
 
   const refPseudo = useRef(null);
   const refEmail = useRef(null);
   const refPassword = useRef(null);
   const refConfirmPassword = useRef(null);
+
+  const toggleVisibilityPassword = useCallback(() => {
+    setVisibilityPassword((current) => !current);
+  }, []);
+
+  const toggleVisibilityConfirmPassword = useCallback(() => {
+    setVisibilityConfirmPassword((current) => !current)
+  }, [])
+
+  const toggleRegister = useCallback(() => {
+    setRegister((current) => !current);
+  }, []);
 
   const login = () => {
     if (refEmail.current.value !== "" && refPassword.current.value !== "") {
@@ -39,13 +54,15 @@ export default function Auth() {
       fetch(url, requestOptions)
         .then((response) => response.json())
         .then((data) => {
-          dispatch(userConnect({
-            token: data.token,
-            username: data.name,
-            email: data.email,
-            id: data.id,
-          })) 
-          navigate("/home", { replace: true })
+          dispatch(
+            userConnect({
+              token: data.token,
+              username: data.name,
+              email: data.email,
+              id: data.id,
+            })
+          );
+          navigate("/home", { replace: true });
         });
     } else {
       setError("Vous devez remplir les deux champs");
@@ -60,6 +77,8 @@ export default function Auth() {
       refConfirmPassword.current.value !== ""
     ) {
       if (refPassword.current.value === refConfirmPassword.current.value) {
+        console.log(refPassword.current.value)
+        console.log(refConfirmPassword.current.value);
         const url = "http://localhost:3001/user";
 
         const requestOptions = {
@@ -73,7 +92,7 @@ export default function Auth() {
         };
         fetch(url, requestOptions)
           .then((response) => response.json())
-          .then((data) => console.log(data));
+          .then(() => login());
       } else {
         setError("Les deux mots de passe ne sont pas identiques");
       }
@@ -82,100 +101,77 @@ export default function Auth() {
     }
   };
 
-  const toggleVisibility = useCallback(() => {
-    setVisibility((current) => !current);
-  }, []);
-
-  const toggleRegister = useCallback(() => {
-    setRegister((current) => !current);
-  }, []);
-
   return (
-    <div className="container d-flex align-items-center justify-content-center">
-      <div>
+    <Stack direction="column" spacing={2} sx={{
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -60%)',
+      zIndex: 1,
+      backgroundColor: 'white',
+      boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.1)',
+      width: '80%',
+      maxWidth: '400px',
+      p: 4,
+      borderRadius: 8
+    }}>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1 },
+          display: 'flex', flexDirection: 'column'
+        }}
+        noValidate
+        autoComplete="off"
+      >
         <h1>{register ? "Inscription" : "Connexion"}</h1>
-        {register && (
-          <div className="mb-3">
-            <FormControl sx={{ m: 1, width: '25ch' }} variant="standard"/>
-          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-          <Input id="standard-adornment-password" type={visibility ? 'text' : 'password'} endAdornment={
+        {register && (<TextField id="standard-basic" label="Pseudo" variant="standard" inputRef={refPseudo}/>)}
+        <TextField id="standard-basic" label="Adresse mail" variant="standard" inputRef={refEmail}/>
+        <FormControl sx={{ m: 1 }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">Mot de passe</InputLabel>
+          <Input
+            inputRef={refPassword}
+            id="standard-adornment-password"
+            type={visibilityPassword ? 'text' : 'password'}
+            endAdornment={
               <InputAdornment position="end">
-                <IconButton aria-label="toggle password visibility" onClick={toggleVisibility}>
-                  {visibility ? <VisibilityOff /> : <Visibility />}
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={toggleVisibilityPassword}
+                >
+                  {visibilityPassword ? <VisibilityOff /> : <Visibility />}
                 </IconButton>
               </InputAdornment>
             }
           />
-          </div>
-          
-        )}
-        <div className="mb-3">
-          <label className="form-label">Adresse mail</label>
-          <input
-            ref={refEmail}
-            type="email"
-            className="form-control"
-            placeholder="name@example.com"
-          />
-        </div>
-        <label className="form-label">Mot de passe</label>
-        <div className="input-group mb-3">
-          <input
-            ref={refPassword}
-            type={visibility ? "text" : "password"}
-            id="inputPassword5"
-            className="form-control"
-            aria-labelledby="passwordHelpBlock"
-          />
-          <div className="input-group-append">
-            <button onClick={toggleVisibility} className="btn btn-outline-dark">
-              <EyeFill />
-            </button>
-          </div>
-        </div>
-        {register && (
-          <div className="mb-3">
-            <label className="form-label">Confirmation</label>
-            <div className="input-group mb-3">
-              <input
-                ref={refConfirmPassword}
-                type={visibility ? "text" : "password"}
-                id="inputPassword5"
-                className="form-control"
-                aria-labelledby="passwordHelpBlock"
-              />
-              <div className="input-group-append">
-                <button
-                  onClick={toggleVisibility}
-                  className="btn btn-outline-dark"
+        </FormControl>
+        {register && (<FormControl sx={{ m: 1 }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">Confirmation</InputLabel>
+          <Input
+            inputRef={refConfirmPassword}
+            id="standard-adornment-password"
+            type={visibilityConfirmPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={toggleVisibilityConfirmPassword}
                 >
-                  <EyeFill />
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
-        <div className="text-center">
-          <button
-            type="button"
-            className="btn btn-dark"
-            onClick={register ? signup : login}
-          >
-            {register ? "Inscription" : "Connexion"}
-          </button>
+                  {visibilityConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>)}
+        <Button variant="contained" onClick={register ? signup : login}>{register ? "Inscription" : "Connexion"}</Button>
+
+        <div style={{ textAlign: 'center' }}>
+          {register ? <p>Déjà un compte ? <span style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={toggleRegister}>Connectez-vous</span></p> : <p>Pas encore de compte ? <span style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={toggleRegister}>Inscrivez-vous</span></p> }
         </div>
-        <p>
-          {register ? "Déjà un compte ? " : "Pas encore de compte ? "}
-          <span
-            className="text-decoration-underline"
-            role="button"
-            onClick={toggleRegister}
-          >
-            {register ? "Connectez-vous" : "Inscrivez-vous"}
-          </span>
-        </p>
-        {error !== "" && <p className="text-danger">Erreur : {error}</p>}
-      </div>
-    </div>
+
+        {error && <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>}
+
+      </Box>
+    </Stack>
   );
-        }
+}
--- src/components/nav.js
+++ src/components/nav.js
@@ -2,13 +2,42 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import { useDispatch, useSelector } from 'react-redux'; 
+import { userDisconnect } from '../store'
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const user = useSelector(state => state);
     const [value, setValue] = React.useState('accueil');
   
     const handleChange = (event, newValue) => {
       setValue(newValue);
     };
+
+    const openProfilMenu = () => {
+
+    }
+
+    const match = () => {
+
+    }
+
+    const logout = () => {
+      const url = "http://localhost:3001/logout";
+
+      const requestOptions = {
+        method: "POST",
+        headers: { "WWW-Authenticate" : user.token },
+      };
+      fetch(url, requestOptions)
+        .then((data) => {
+          dispatch(userDisconnect())
+          navigate("/auth")
+        }); 
+    }
   
     return (
       <Box sx={{ width: '100%' }}>
@@ -20,9 +49,8 @@ export default function NavBar() {
           aria-label="secondary tabs example"
         >
           <Tab value="accueil" label="Accueil" />
-          <Tab value="login" label="Login" />
-          <Tab value="Connexion" label="Connexion" />
-          <Tab value="Deconnexion" label="Deconnexion" />
+          <Tab value="match" label="Match" onClick={match}/>
+          <Tab value="profil" label="Profil" onClick={logout}/>
         </Tabs>
       </Box>
     );
